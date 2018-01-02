3b5af2949e9075c5487051425c31bfe6fe7e6bfa
emily <limeyb7@gmail.com>
2018-01-02 20:45:21 +0000

    hello world

---
+++ mix.exs
@@ -0,0 +1,29 @@
+defmodule Helloplug.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :helloplug,
+      version: "0.1.0",
+      elixir: "~> 1.5",
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {Server, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:cowboy, "~> 1.1.2"},
+      {:plug, "~> 1.3.4"}
+    ]
+  end
+end
